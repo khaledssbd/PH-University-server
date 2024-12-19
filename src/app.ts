c1b135688177680c770a9f03675966d37872e542
khaledssbd <khaledssbd@gmail.com>
2024-12-19 12:34:02 +0600

    part-7.2

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,23 @@ import router from './app/routes';
 
 const app: Application = express();
 
+const corsConfig = {
+  origin: [
+    'http://localhost:5173',
+    // 'http://localhost:5174',
+    // 'https://urbanutopia-by-khaled.web.app',
+    // 'https://urbanutopia-by-khaled.vercel.app',
+    // 'https://urbanutopia-by-khaled.surge.sh',
+    // 'https://urbanutopia-by-khaled.netlify.app',
+  ],
+  // credentials: true,
+};
+
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+// app.use(cors
+app.use(cors(corsConfig));
 
 
 // api/v1/students/create-student
